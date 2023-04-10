aa4f41e67e8292ed9fd6afff5d21f0fcfe35d1d0
purin <hwijae0701@gmail.com>
2023-04-10 21:00:17 +0900

    feat: 팀 렌더링 구현

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,8 +1,15 @@
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+
 function Settings(){
+  const [teams,setTeams]=useState(["멘유",'바르셀로나','레알마드리드']);
   return(
-    <>
-      Settings
-    </>
+    <Box>
+      <Typography variant="h1">Settings</Typography>
+      {
+        teams.map((team)=> <Box>{team}</Box>)
+      }
+    </Box>
   )
 }
 export default Settings;

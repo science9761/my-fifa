1e59fcfee8ba24862c0eb11e9934ce311a06d42e
purin <hwijae0701@gmail.com>
2023-06-11 11:31:32 +0900

    feat: 선수 추가 모달 구현
    Co-authored-by: Gabriel Ju Hyun, Yoon <gabrielyoon7@gmail.com>

--- src/pages/Settings/Players/Players.jsx
+++ src/pages/Settings/Players/Players.jsx
@@ -1,20 +1,36 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Dialog } from "@mui/material";
 import { useState } from "react";
 import { defaultPlayers } from "../../../data/defaultPlayers.jsx";
 import PlayerCard from "../../../components/PlayerCard.jsx";
 
 function Players() {
     const [players, setPlayers] = useState(defaultPlayers);
-    const deletePlayer = (name) => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const deletePlayer = (name) => {        
         const removedPlayers = players.filter((p) => p.name !== name);
         setPlayers(removedPlayers);
     }
 
+    const handleModalOpen = () => {
+        setModalOpen(!modalOpen);
+    }
+
     return (
+    <>
+        <Button
+            variant="contained"
+            color="success"
+            onClick={()=>handleModalOpen()}
+        >
+            선수 추가
+        </Button>
         <Box>
             {players.map((player) => <PlayerCard player={player} />)}
         </Box>
-
+        <Dialog onClose={handleModalOpen} open={modalOpen}>
+            푸리니
+        </Dialog>
+    </>
     )
 }
 export default Players;

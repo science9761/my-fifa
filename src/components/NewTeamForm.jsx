50d2fdb6a7c83c8720902879e3d71004fea6a91f
purin <hwijae0701@gmail.com>
2023-06-11 11:31:48 +0900

    feat: 팀 등록 폼 뼈대 추가

---
+++ src/components/NewTeamForm.jsx
@@ -0,0 +1,16 @@
+import { Stack, TextField } from "@mui/material";
+
+function NewTeamForm(){
+    return(
+        <>
+            <Stack sx={{padding:'10px'}} gap={2}>
+              <TextField label="Outlined" variant="outlined" />
+              <TextField label="Outlined" variant="outlined" />
+            <TextField label="Outlined" variant="outlined" />
+            <TextField label="Outlined" variant="outlined" />
+            </Stack>
+        </>
+    )
+}
+
+export default NewTeamForm;
--- src/pages/Settings/Teams/Teams.jsx
+++ src/pages/Settings/Teams/Teams.jsx
@@ -1,40 +1,41 @@
-import { Box, Button, Dialog, Typography } from "@mui/material";
+import { Box, Button, Dialog, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { defaultTeams } from "../../../data/defaultTeams.jsx";
 import TeamCard from "../../../components/TeamCard.jsx";
+import NewTeamForm from "../../../components/NewTeamForm.jsx";
 
 function Teams() {
   const [teams, setTeams] = useState(defaultTeams);
-  const [open, setOpen] = useState(false);
-
+  const [modalOpen, setModalOpen] = useState(false);
+  
   const deleteTeam = (name) => {
     const removedTeams = teams.filter((t) => t.name !== name);
     setTeams(removedTeams);
   }
 
-  const handleClose = () => {
-    setOpen(!open);
+  const handleModalOpen = () => {
+    setModalOpen(!modalOpen);
   }
 
   return (
     <>
-    <Button 
-      onClick={handleClose}
-      variant="contained"
-      color="success"
-    >
-      팀 추가
-    </Button>
-    <Box sx={{margin:'10px'}}>
-      {
-        teams.map((team,i) => (
-          <TeamCard team={team} key={i}/>
-        ))
-      }
-    </Box>
-    <Dialog onClose={handleClose} open={open}>
-      하이
-    </Dialog>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={()=>handleModalOpen()}
+      >
+        팀 추가
+      </Button>
+      <Box sx={{margin:'10px'}}>
+        {
+          teams.map((team,i) => (
+            <TeamCard team={team} key={i}/>
+          ))
+        }
+      </Box>
+      <Dialog onClose={handleModalOpen} open={modalOpen}>
+        <NewTeamForm/>
+      </Dialog>
     </>
   )
 }
